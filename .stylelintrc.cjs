697e4c1f913ee04c5c11efd8c98f83938bc270b5
吴东 <511970747@qq.com>
2023-12-18 10:31:24 +0800

    完善2

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,20 +1,17 @@
 module.exports = {
   // 继承的预设，这些预设包含了规则集插件
   extends: [
-    // 代码风格规则
-    'stylelint-stylistic/config',
+    // 样式属性顺序规则
+    'stylelint-config-recess-order',
     // 基本 scss 规则
     'stylelint-config-standard-scss',
     // scss vue 规则
     'stylelint-config-recommended-vue/scss',
-    // 样式属性顺序规则
-    'stylelint-config-recess-order'
+    "stylelint-prettier/recommended"
   ],
   rules: {
     'selector-class-pattern': null,
     'scss/at-import-no-partial-leading-underscore': null,
-    'string-quotes': 'single',
-    'function-url-quotes': 'single',
-    'font-family-name-quotes': 'single'
+    "prettier/prettier": [true, {"singleQuote": true,}]
   }
 }
